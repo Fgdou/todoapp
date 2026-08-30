d53b5484a05b7f040345099e014bb2609e22e650
Fgdou <fabigoardou@gmail.com>
2026-08-30 11:33:20 +0200

    feat: material ui

---
+++ todoapp_frontend/src/components/todo_list.rs
@@ -0,0 +1,45 @@
+use yew::{platform::spawn_local, prelude::*};
+
+use crate::{api::get_todos, models::todos::Todo};
+
+#[derive(Properties, PartialEq)]
+struct ItemProps {
+    item: Todo,
+}
+
+#[component]
+fn Item(props: &ItemProps) -> Html {
+    let item = &props.item;
+    html!(
+        <div>
+            <span><md-checkbox touch-target="wrapper"/></span>
+            <span>{item.title.clone()}</span>
+            <span>{item.description.clone()}</span>
+        </div>
+    )
+}
+
+#[component]
+pub fn TodoList() -> Html {
+    let todos = use_state(|| Vec::new());
+
+    {
+        let todos = todos.clone();
+        use_effect_with((), move |_| {
+            spawn_local(async move {
+                let res = get_todos().await;
+                todos.set(res);
+            });
+
+            || ()
+        });
+    }
+
+    html!(
+        <div>
+            for todo in todos.iter() {
+                <Item item={todo.clone()}/>
+            }
+        </div>
+    )
+}
--- todoapp_frontend/src/main.rs
+++ todoapp_frontend/src/main.rs
@@ -1,57 +1,18 @@
 pub mod models;
 pub mod api;
+pub mod components;
 
-use yew::{platform::spawn_local, prelude::*};
+use yew::prelude::*;
 
-use crate::{api::get_todos, models::todos::Todo};
-
-#[derive(Properties, PartialEq)]
-struct ItemProps {
-    item: Todo,
-}
-
-#[component]
-fn Item(props: &ItemProps) -> Html {
-    let item = &props.item;
-    html!(
-        <div>
-            <span>{item.done}</span>
-            <span>{item.title.clone()}</span>
-            <span>{item.description.clone()}</span>
-        </div>
-    )
-}
-
-#[component]
-fn List() -> Html {
-    let todos = use_state(|| Vec::new());
-
-    {
-        let todos = todos.clone();
-        use_effect_with((), move |_| {
-            spawn_local(async move {
-                let res = get_todos().await;
-                todos.set(res);
-            });
-
-            || ()
-        });
-    }
-
-    html!(
-        <div>
-            <h1>{"Todo List"}</h1>
-            for todo in todos.iter() {
-                <Item item={todo.clone()}/>
-            }
-        </div>
-    )
-}
+use crate::components::todo_list::TodoList;
 
 #[component]
 fn App() -> Html {
     html!(
-        <List/>
+        <div>
+            <h1>{"TODO List"}</h1>
+            <TodoList/>
+        </div>
     )
 }
 
