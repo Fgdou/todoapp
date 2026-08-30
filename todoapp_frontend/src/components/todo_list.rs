195f5af83e5ca98b2ac1970097326171bf520453
Fgdou <fabigoardou@gmail.com>
2026-08-30 12:40:16 +0200

    feat: update fields

--- todoapp_frontend/src/components/todo_list.rs
+++ todoapp_frontend/src/components/todo_list.rs
@@ -29,12 +29,47 @@ fn Item(props: &ItemProps) -> Html {
         )
     };
 
+    let title = {
+        let item = item.clone();
+        let title = item.title.clone();
+        let done = item.done;
+        let update_item_callback = props.update_item_callback.clone();
+
+        let callback = Callback::from(move |value| {
+            let mut item = item.clone();
+            let update_item_callback = update_item_callback.clone();
+            item.title = value;
+            update_item_callback.map(|c| c.emit(item));
+        });
+
+        html!(
+            <HiddenInput value={title} strikethrough={done} callback={callback} />
+        )
+    };
+
+    let description = {
+        let item = item.clone();
+        let description = item.description.clone();
+        let update_item_callback = props.update_item_callback.clone();
+
+        let callback = Callback::from(move |value| {
+            let mut item = item.clone();
+            let update_item_callback = update_item_callback.clone();
+            item.description = value;
+            update_item_callback.map(|c| c.emit(item));
+        });
+
+        html!(
+            <HiddenInput value={description} callback={callback} />
+        )
+    };
+
 
     html!(
         <div>
             <span>{checkbox}</span>
-            <span><HiddenInput value={item.title.clone()} strikethrough={item.done} /></span>
-            <span><HiddenInput value={item.description.clone()} /></span>
+            <span>{title}</span>
+            <span>{description}</span>
         </div>
     )
 }
