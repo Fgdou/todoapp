a0f3e937d5d5b3672e895a2f0d4063f8de0701df
Fgdou <fabigoardou@gmail.com>
2026-08-30 10:07:07 +0200

    feat: CRUD

--- todoapp_backend/src/main.rs
+++ todoapp_backend/src/main.rs
@@ -1,17 +1,32 @@
 #[macro_use] extern crate rocket;
 
 use dotenvy::dotenv;
-use rocket::{response::status, serde::json::Json};
+use rocket::{serde::json::Json};
 use rocket_sync_db_pools::database;
 
-use crate::models::todos::Todo;
+use crate::models::todos::{Todo, TodoInsert};
 
 pub mod schema;
 pub mod models;
 
 #[get("/")]
-async fn list_todos(conn: Database) -> status::Accepted<Json<Vec<Todo>>> {
-    status::Accepted(Json(Todo::get_all(&conn).await))
+async fn list_todos(conn: Database) -> Json<Vec<Todo>> {
+    Json(Todo::get_all(&conn).await)
+}
+
+#[post("/", data = "<todo>")]
+async fn create_todo(conn: Database, todo: Json<TodoInsert>) -> Json<Todo> {
+    Json(todo.save(&conn).await)
+}
+
+#[put("/", data = "<todo>")]
+async fn update_todo(conn: Database, todo: Json<Todo>) {
+    todo.update(&conn).await
+}
+
+#[delete("/<id>")]
+async fn delete_todo(conn: Database, id: i32) {
+    Todo::delete(&conn, id).await
 }
 
 #[database("sqlite")]
@@ -22,5 +37,5 @@ fn rocket() -> _ {
     dotenv().ok();
     rocket::build()
         .attach(Database::fairing())
-        .mount("/", routes![list_todos])
+        .mount("/", routes![list_todos, create_todo, update_todo, delete_todo])
 }
--- todoapp_backend/src/models/todos.rs
+++ todoapp_backend/src/models/todos.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::{Database, schema::todos};
 
-#[derive(Queryable, Selectable, Deserialize, Serialize)]
+#[derive(Queryable, Selectable, Deserialize, Serialize, AsChangeset, Clone)]
 #[diesel(table_name = crate::schema::todos)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Todo {
@@ -13,11 +13,48 @@ pub struct Todo {
     pub done: bool,
 }
 
+#[derive(Insertable, Deserialize, Clone)]
+#[diesel(table_name = crate::schema::todos)]
+pub struct TodoInsert {
+    pub title: String,
+    pub description: String,
+}
+
 
 impl Todo {
     pub async fn get_all(conn: &Database) -> Vec<Self> {
         conn.run(move |conn| {
             todos::table.load::<Self>(conn).unwrap()
+        }).await
+    }
+    pub async fn update(&self, conn: &Database) {
+        let me = self.clone();
+        conn.run(move |conn| {
+            diesel::update(todos::table)
+                .set(me)
+                .execute(conn)
+                .unwrap();
+        }).await
+    }
+    pub async fn delete(conn: &Database, id: i32) {
+        conn.run(move |conn| {
+            diesel::delete(todos::table)
+                .filter(todos::id.eq(id))
+                .execute(conn)
+                .unwrap();
+        }).await
+    }
+}
+
+impl TodoInsert {
+    pub async fn save(&self, conn: &Database) -> Todo {
+        let me = self.clone();
+        conn.run(move |conn| {
+            diesel::insert_into(todos::table)
+                .values(me)
+                .returning(Todo::as_returning())
+                .get_result(conn)
+                .unwrap()
         }).await
     }
 }
