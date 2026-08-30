86e428124fe99ca66f811b6fa02f7efb13530daf
Fgdou <fabigoardou@gmail.com>
2026-08-30 12:35:45 +0200

    feat: update checkbox

--- todoapp_frontend/src/api.rs
+++ todoapp_frontend/src/api.rs
@@ -5,8 +5,18 @@ use crate::models::todos::Todo;
 const BACKEND_URL: &str = dotenv!("BACKEND_URL");
 
 pub async fn get_todos() -> Vec<Todo> {
-    let url = format!("{BACKEND_URL}/");
+    let url = BACKEND_URL;
     reqwest::get(url)
         .await.unwrap()
         .json::<_>().await.unwrap()
+}
+
+pub async fn update_todo(todo: Todo) {
+    let url = BACKEND_URL;
+    reqwest::Client::new()
+        .put(url)
+        .json(&todo)
+        .send()
+        .await
+        .unwrap();
 }
--- todoapp_frontend/src/components/todo_list.rs
+++ todoapp_frontend/src/components/todo_list.rs
@@ -1,18 +1,38 @@
 use yew::{platform::spawn_local, prelude::*};
 
-use crate::{api::get_todos, components::{checkbox::Checkbox, hidden_input::HiddenInput}, models::todos::Todo};
+use crate::{api::{get_todos, update_todo}, components::{checkbox::Checkbox, hidden_input::HiddenInput}, models::todos::Todo};
 
 #[derive(Properties, PartialEq)]
 struct ItemProps {
     item: Todo,
+    #[prop_or_default]
+    update_item_callback: Option<Callback<Todo>>,
 }
 
 #[component]
 fn Item(props: &ItemProps) -> Html {
     let item = &props.item;
+
+    let checkbox = {
+        let item = item.clone();
+        let done = item.done;
+        let update_item_callback = props.update_item_callback.clone();
+        let callback = Callback::from(move |value| {
+            let mut item = item.clone();
+            let update_item_callback = update_item_callback.clone();
+            item.done = value;
+            update_item_callback.map(|c| c.emit(item));
+        });
+
+        html!(
+            <Checkbox checked={done} on_change={callback}/>
+        )
+    };
+
+
     html!(
         <div>
-            <span><Checkbox checked={item.done}/></span>
+            <span>{checkbox}</span>
             <span><HiddenInput value={item.title.clone()} strikethrough={item.done} /></span>
             <span><HiddenInput value={item.description.clone()} /></span>
         </div>
@@ -35,10 +55,28 @@ pub fn TodoList() -> Html {
         });
     }
 
+    let callback = {
+        let todos = todos.clone();
+        Callback::from(move |todo: Todo| {
+            {    
+                let todo = todo.clone();
+                spawn_local(async move {
+                    update_todo(todo.clone()).await;
+                });
+            }
+
+            let mut new_todos = (*todos).clone();
+            let found = new_todos.iter_mut().find(|i| i == &&todo).unwrap();
+            *found = todo;
+
+            todos.set(new_todos);
+        })
+    };
+
     html!(
         <div  class="flex gap-3 flex-col">
             for todo in todos.iter() {
-                <Item item={todo.clone()}/>
+                <Item item={todo.clone()} update_item_callback={callback.clone()}/>
             }
         </div>
     )
