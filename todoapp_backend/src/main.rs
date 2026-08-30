24c966c469241bd5c62cfdd0c036c6c6bd525640
Fgdou <fabigoardou@gmail.com>
2026-08-30 16:18:51 +0200

    feat: migration at startup

--- todoapp_backend/src/main.rs
+++ todoapp_backend/src/main.rs
@@ -1,7 +1,8 @@
 #[macro_use] extern crate rocket;
 
+use diesel_migrations::{EmbeddedMigrations, MigrationHarness, embed_migrations};
 use dotenvy::dotenv;
-use rocket::{serde::json::Json};
+use rocket::{fairing::AdHoc, serde::json::Json};
 use rocket_cors::{AllowedOrigins, CorsOptions};
 use rocket_sync_db_pools::database;
 
@@ -9,6 +10,8 @@ use crate::models::todos::{Todo, TodoInsert};
 
 pub mod schema;
 pub mod models;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 #[get("/")]
 async fn list_todos(conn: Database) -> Json<Vec<Todo>> {
@@ -35,6 +38,8 @@ pub struct Database(diesel::SqliteConnection);
 
 #[launch]
 fn rocket() -> _ {
+    println!("Starting backend");
+
     let allowed_origins = AllowedOrigins::some_exact(&[
         "http://localhost:8080",
     ]);
@@ -55,5 +60,29 @@ fn rocket() -> _ {
     rocket::build()
         .attach(cors)
         .attach(Database::fairing())
+        .attach(AdHoc::on_ignite("Database Migrations", |rocket| async move {
+            println!("Running database Migrations...");
+            // 1. Extract the SQLite database connection string from Rocket's config
+            let db_url = rocket.figment()
+                .extract_inner::<String>("databases.sqlite.url")
+                .expect("Missing database URL in config");
+
+            // 2. Spawn a blocking thread safely outside Rocket's async pool
+            rocket::tokio::task::spawn_blocking(move || {
+                use diesel::prelude::*;
+
+                let mut conn = SqliteConnection::establish(&db_url)
+                    .expect("Failed to connect to SQLite database for migrations");
+
+                conn.run_pending_migrations(MIGRATIONS)
+                    .expect("Failed to run SQLite migrations");
+            })
+            .await
+            .expect("Migration task failed");
+
+            println!("Done !");
+
+            rocket
+        }))
         .mount("/", routes![list_todos, create_todo, update_todo, delete_todo])
 }
