3168be69b5b6e7477fa732de647883e6765919e5
Fgdou <fabigoardou@gmail.com>
2026-08-31 16:12:27 +0200

    feat: full login

--- todoapp_frontend/src/api.rs
+++ todoapp_frontend/src/api.rs
@@ -5,18 +5,20 @@ use crate::models::{tasks::{Task, TaskInsert}, users::{Login, User}};
 
 const BACKEND_URL: &str = dotenv!("BACKEND_URL");
 
-pub async fn get_tasks() -> Vec<Task> {
+pub async fn get_tasks(token: &str) -> Vec<Task> {
     let url = format!("{BACKEND_URL}/tasks/");
     Request::get(&url)
+        .header("Token", token)
         .send()
         .await
         .unwrap()
         .json::<_>().await.unwrap()
 }
 
-pub async fn update_task(todo: Task) {
+pub async fn update_task(todo: Task, token: &str) {
     let url = format!("{BACKEND_URL}/tasks/");
     Request::put(&url)
+        .header("Token", token)
         .json(&todo)
         .unwrap()
         .send()
@@ -24,9 +26,10 @@ pub async fn update_task(todo: Task) {
         .unwrap();
 }
 
-pub async fn new_task(todo: TaskInsert) -> Task {
+pub async fn new_task(todo: TaskInsert, token: &str) -> Task {
     let url = format!("{BACKEND_URL}/tasks/");
     Request::post(&url)
+        .header("Token", token)
         .json(&todo)
         .unwrap()
         .send()
@@ -37,9 +40,10 @@ pub async fn new_task(todo: TaskInsert) -> Task {
         .unwrap()
 }
 
-pub async fn delete_task(id: i32) {
+pub async fn delete_task(id: i32, token: &str) {
     let url = format!("{BACKEND_URL}/tasks/{id}");
     Request::delete(&url)
+        .header("Token", token)
         .send()
         .await
         .unwrap();
--- todoapp_frontend/src/components/task_list.rs
+++ todoapp_frontend/src/components/task_list.rs
@@ -1,16 +1,28 @@
 use yew::{platform::spawn_local, prelude::*};
+use yew_router::hooks::use_navigator;
 
-use crate::{api::{delete_task, get_tasks, new_task, update_task}, components::task::TaskItem, models::tasks::{Task, TaskInsert}};
+use crate::{Route, api::{delete_task, get_tasks, new_task, update_task}, components::task::TaskItem, models::{context::AppContext, tasks::{Task, TaskInsert}}};
 
 #[component]
 pub fn TaskList() -> Html {
     let tasks = use_state(|| Vec::new());
+    let context = use_context::<AppContext>().unwrap();
+    let user = &context.user;
+    let token = user.as_ref().map(|u| u.token.clone()).unwrap_or_default();
+    let navigator = use_navigator().unwrap();
+
+    if user.as_ref().is_none() {
+        navigator.push(&Route::Login);
+        return html!(<></>)
+    }
 
     {
         let tasks = tasks.clone();
+        let token = token.clone();
         use_effect_with((), move |_| {
+            let token = token.clone();
             spawn_local(async move {
-                let res = get_tasks().await;
+                let res = get_tasks(&token).await;
                 tasks.set(res);
             });
 
@@ -20,11 +32,13 @@ pub fn TaskList() -> Html {
 
     let callback = {
         let tasks = tasks.clone();
+        let token = token.clone();
         Callback::from(move |todo: Task| {
             {    
                 let todo = todo.clone();
+            let token = token.clone();
                 spawn_local(async move {
-                    update_task(todo.clone()).await;
+                    update_task(todo.clone(), &token).await;
                 });
             }
 
@@ -38,9 +52,11 @@ pub fn TaskList() -> Html {
 
     let delete_callback = {
         let tasks = tasks.clone();
+        let token = token.clone();
         Callback::from(move |id: i32| {
+            let token = token.clone();
             spawn_local(async move {
-                delete_task(id).await;
+                delete_task(id, &token).await;
             });
 
             let mut new_tasks = (*tasks).clone();
@@ -51,15 +67,17 @@ pub fn TaskList() -> Html {
 
     let new_item = {
         let tasks = tasks.clone();
+        let token = token.clone();
         Callback::from(move |_| {
             let tasks = tasks.clone();
+            let token = token.clone();
             spawn_local(async move {
                 let mut new_tasks = (*tasks).clone();
                 let item = TaskInsert {
                     description: String::new(),
                     title: String::new(),
                 };
-                let res = new_task(item).await;
+                let res = new_task(item, &token).await;
                 new_tasks.push(res);
                 tasks.set(new_tasks);
             });
@@ -68,10 +86,12 @@ pub fn TaskList() -> Html {
 
     let update_handler = {
         let tasks = tasks.clone();
+        let token = token.clone();
         Callback::from(move |_| {
             let tasks = tasks.clone();
+            let token = token.clone();
             spawn_local(async move {
-                let items = get_tasks().await;
+                let items = get_tasks(&token).await;
                 tasks.set(items);
             });
         })
